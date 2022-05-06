088689b49fd62b9d1714e14fc35591c0bafd4950
juan carlos llanos <o.llanos.juan19@gmail.com>
2022-05-05 20:51:55 -0500

    dashboar y estilos del dashboard

---
+++ Dashboard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>prueba</title>
+        <link rel="stylesheet" href="CSS/prueba.css">
+    </head>
+    <body>
+        <div id="userInput">
+            <input id="textInput" class="input-box" type="text" name="msg" placeholder="Pregunta">
+            <p></p>
+        </div>
+        <div id="userInput">
+            <input id="textInput" class="input-box" type="text" name="msg" placeholder="Respuesta">
+            <p></p>
+        </div>
+        <label class="control-label">Destinatario</label>
+						<div class="controls">
+							<select name="idDestinatario">
+								<option disabled selected>Ingrese un usuario</option>
+								<?php
+									$pdo = Database::connect();
+									$query = 'SELECT * FROM pag4_usuario';
+									foreach($pdo->query($query) as $row){
+										if ($row['idUsuario']==$idDestinatario)
+											echo "<option selected value'" . $row['idUsuario'] . "'>" . $row['usuario'] . "</option>";
+										else
+											echo "<option value='" . $row['idUsuario'] . "'>" . $row['usuario'] . "</option>";
+									}
+									Database::disconnect();
+								?>
+							</select>
+        <button type="button" role="button" href ="html/maximized.html" class="ac-pushButton">
+            <div>Enviar</div>
+        </button>
+    </body>
+</html>
